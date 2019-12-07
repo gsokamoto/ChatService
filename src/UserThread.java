aa9954e7858828bbac8d2bd2d227e6e95882619f
Grant Okamoto <gsokamoto@cpp.edu>
2019-12-07 11:22:14 -0800

    Chat room fully functioning. Still need to: test, modify code, add time stamp, add GUI module, possibly make output look nice for console

--- src/UserThread.java
+++ src/UserThread.java
@@ -13,11 +13,49 @@ public class UserThread extends Thread {
     }
 
     // allow this thread to broadcast chat server
-    public void run(){}
+    public void run(){
+        try {
+            InputStream input = socket.getInputStream();
+            BufferedReader reader = new BufferedReader((new InputStreamReader(input)));
+
+            OutputStream output = socket.getOutputStream();
+            writer = new PrintWriter(output, true);
+
+            printUsers();
+
+            String userName = reader.readLine();
+            server.addUserName(userName);
+
+            String serverMessage = "New user connected: " + userName;
+            server.broadcast(serverMessage, this);
+
+            String clientMessage;
+
+            do {
+                clientMessage = reader.readLine();
+                serverMessage = "[" + userName + "]: " + clientMessage;
+                server.broadcast(serverMessage, this);
+            } while (!clientMessage.equals("."));
+
+        } catch (IOException ex) {
+            System.out.println("Error in UserThread: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
 
     // print users on console, or send as string for GUI later
-    public void printUsers(){}
+    public void printUsers(){
+        if(server.hasUsers()){
+            if (server.hasUsers()) {
+                writer.println("Connected users: " + server.getUserNames());
+            } else {
+                writer.println("No other users connected");
+            }
+        }
+    }
 
     // sends message to client
-    void sendMessage(String message) {}
+    void sendMessage(String message) {
+        writer.println(message);
+    }
 }
